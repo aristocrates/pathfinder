b1a22fe0f413689a1cb8449608f6f97e509dfd53
Nicholas Meyer <nickAristocrates@gmail.com>
2018-06-27 11:28:08 -0700

    Save work on gui

--- gui-qt.py
+++ gui-qt.py
@@ -7,6 +7,18 @@ Written with reference to http://zetcode.com/gui/pyqt5
 """
 
 import sys
+import PyQt5
+
+# events
+from PyQt5.QtCore import QEvent
+MouseMove           = QEvent.MouseMove
+MouseButtonPress    = QEvent.MouseButtonPress
+MouseButtonRelease  = QEvent.MouseButtonRelease
+MouseButtonDblClick = QEvent.MouseButtonDblClick
+from PyQt5.QtCore import Qt
+LeftButton  = Qt.LeftButton
+RightButton = Qt.RightButton
+
 from PyQt5.QtWidgets import QApplication, QDesktopWidget, QWidget, QAction
 from PyQt5.QtWidgets import QMainWindow, QMenu, QVBoxLayout
 from PyQt5.QtSvg import QSvgWidget
@@ -51,6 +63,12 @@ class CenteredWindow(QMainWindow):
         xmin, xmax, ymin, ymax = paths[0].bbox()
         self.resize((xmax - xmin) / 1.25, (ymax - ymin) / 1.25)
 
+        # self.graphicsView = PyQt5.QtWidgets.QGraphicsView()
+        # self.graphicsView.setMouseTracking(True)
+        svgMap.setMouseTracking(True)
+        svgMap.installEventFilter(self)
+        #self.graphicsView.viewport().installEventFilter(self)
+
     def open_map(self):
         pass
 
@@ -59,6 +77,40 @@ class CenteredWindow(QMainWindow):
 
     def save(self):
         pass
+
+    # hash storing all mouse events
+    mouse_events = [MouseMove, MouseButtonPress, MouseButtonRelease,
+                    MouseButtonDblClick]
+    mouse_buttons = [LeftButton, RightButton]
+    def eventFilter(self, source, event):
+        """
+        Written with reference to https://stackoverflow.com/questions/35992088/why-mousemoveevent-does-nothing-in-pyqt5
+        """
+        print(event.type())
+        # if event.type() == PyQt5.QtCore.QEvent.MouseMove:
+        #     print("Mouse Move")
+        # elif event.type() == PyQt5.QtCore.QEvent.MouseButtonPress:
+        #     if event.buttons() == PyQt5.QtCore.Qt.LeftButton:
+        #         print("Left press")
+        #     elif event.buttons() == PyQt5.QtCore.Qt.RightButton:
+        #         print("Right press")
+        # elif event.type() == PyQt5.QtCore.QEvent.MouseButtonRelease:
+        #     if event.buttons() == PyQt5.QtCore.Qt.LeftButton:
+        #         print("Left release")
+        #     elif event.buttons() == PyQt5.QtCore.Qt.RightButton:
+        #         print("Right release")
+        # elif event.type() == PyQt5.QtCore.QEvent.MouseButtonDblClick:
+        #     print("Double click")
+        return super(QMainWindow, self).eventFilter(source, event)
+
+    # def mouseMoveEvent(self, event):
+    #     print("Mouse move event")
+
+    # def mousePressEvent(self, event):
+    #     if event.button() == PyQt5.QtCore.Qt.LeftButton:
+    #         print("Left press")
+    #     elif event.button() == PyQt5.QtCore.Qt.RightButton:
+    #         print("Right press")
 
     def center(self):
         frame_geo  = self.frameGeometry()
